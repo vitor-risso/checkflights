9d82b0febb21f77b596cc6644fc98a4452b49e57
Vitor Hugo Risso Da Silva <vitorhugorissodasilva@MacBook-Air-de-Vitor.local>
2025-08-03 17:54:33 -0300

    user agent change

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function buscarVoos(origem, destino, data) {
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-site",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
     };
 
     try {
